7026c3972a8440cd4a8abeb095fdb29ce2fc1b1b
Aquarices <38940188+Aquarices@users.noreply.github.com>
2019-03-28 18:58:12 +0800

    Update AmbientLux_main.py

--- AmbientLux_main.py
+++ AmbientLux_main.py
@@ -2,47 +2,33 @@
 #coding:utf-8
 import os, sys
 
-#Name:saveString
-#Description:保存每一行的结果
-def saveString(string):
-   line = "%s\n"%(string)
-   with
-
 #Name:bugreportCut
 #Description:去除多余的log信息，保留和调光相关的内容
-def bugreportCut(bugreport):
-   #定义相关数组
-   output = []
-   lineTemp = []
-   lastTemp = []
-   #按“行”打开
-   with open(bugreport, encoding='utf-8') as inPutFile:
-      mdFile = inPutFile.readlines()
-   #保存上传结果
-   resultFile = bugreport + "_output.csv"
-   
-    = open(bugreport, encoding='utf-8')
+def bugreportCut(bugreport):   
+   inPutFile = open(bugreport, encoding='utf-8')
    outPutFile = open(bugreport +'_result.txt', 'w')
    print('Create'+ bugreport + '_result.txt success\n')
    print('Start to read lines...\n')
    lines = inPutFile.readlines()
    for line in lines:
       if "D AutomaticBrightnessController" in line:
-         outPutFile.write(line)
+	item = [line.count(" "), line.lstrip()]
+	if lastTemp == []:lastTemp = item
+		if item[0] > lastTemp[0]:
+			
+		
+        outPutFile.write(line)
 		 
    inPutFile.close()
    outPutFile.close()
          
 
 #-----主程序入口-----
-
 #获取当前目录路径
 dirPath = os.path.dirname(os.path.abspath(__file__))
-
 #按降序对文件排序,排除掉程序自己，从而处理其他bugreport文件
 dirs = os.listdir(dirPath)
 del dirs[0]
-
 #开始处理bugreport
 for file in dirs:
    #去除多余的log信息，保留和调光相关的内容
